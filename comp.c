01fa1fc2e9f0c90d253596a59e0856155912c54c
folejn <folejn@gmail.com>
2020-03-06 21:50:25 +0100

    Sasiedztwo Moore a i von Neumanna

---
+++ comp.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include "generation.h"
+int isInFrame(int r, int c, gener_t *g)
+{
+	int rg = g->r, cg = g->c;
+	if(r >= 0 && r < rg && c >= 0 && c < cg)
+		return 1;
+	return 0; 
+}
+int isAlive4(int r, int c, gener_t *g)
+{
+	int alNeigh = 0;
+	
+	if(isInFrame(r,c-1,g))
+		if(g->cont[r][c-1] == 1)
+			alNeigh++;
+	if(isInFrame(r,c+1,g))
+		if(g->cont[r][c+1] == 1)
+                      alNeigh++;
+	if(isInFrame(r-1,c,g))
+		if(g->cont[r-1][c] == 1)
+                        alNeigh++;
+	if(isInFrame(r+1,c,g))
+		if(g->cont[r+1][c] == 1)
+                        alNeigh++;
+	if(g->cont[r][c] == 0 && alNeigh == 3)  
+		return 1;
+
+	if(g->cont[r][c] == 1 && ( alNeigh == 2 || alNeigh == 3))
+		return 1;
+	return 0;
+}
+int isAlive8(int r, int c, gener_t *g)
+{
+        int alNeigh = 0;
+
+        if(isInFrame(r,c-1,g))
+                if(g->cont[r][c-1] == 1)
+                        alNeigh++;
+        if(isInFrame(r,c+1,g))
+                if(g->cont[r][c+1] == 1)
+                      alNeigh++;
+        if(isInFrame(r-1,c,g))
+                if(g->cont[r-1][c] == 1)
+                        alNeigh++;
+        if(isInFrame(r+1,c,g))
+                if(g->cont[r+1][c] == 1)
+                        alNeigh++;
+	if(isInFrame(r+1,c+1,g))
+                if(g->cont[r+1][c+1] == 1)
+                        alNeigh++;
+	if(isInFrame(r-1,c-1,g))
+                if(g->cont[r-1][c-1] == 1)
+                        alNeigh++;
+	if(isInFrame(r+1,c-1,g))
+                if(g->cont[r+1][c-1] == 1)
+                        alNeigh++;
+	if(isInFrame(r-1,c+1,g))
+                if(g->cont[r-1][c+1] == 1)
+                        alNeigh++;
+        if(g->cont[r][c] == 0 && alNeigh == 3)
+                return 1;
+
+        if(g->cont[r][c] == 1 && ( alNeigh == 2 || alNeigh == 3))
+                return 1;
+        return 0;
+}
+
---
+++ comp.h
@@ -0,0 +1,9 @@
+#ifndef COMP_H
+#define COMP_H
+#include "generation.h"
+
+int isInFrame(int r, int c, gener_t *g);
+int isAlive4(int r, int c, gener_t *g);
+int isAlive8(int r, int c, gener_t *g);
+
+#endif
